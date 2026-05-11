e7c9d5f3032ec66434ae46149d9f18b84a0ea2d2
taehyeooo <dony527@naver.com>
2026-05-11 23:05:32 +0900

    fix: PlaceAnalysisService.java에 @Async 어노테이션 import 누락

--- src/main/java/com/planb/planb_backend/domain/place/service/external/PlaceAnalysisService.java
+++ src/main/java/com/planb/planb_backend/domain/place/service/external/PlaceAnalysisService.java
@@ -9,6 +9,7 @@ import com.planb.planb_backend.domain.trip.entity.PlaceType;
 import com.planb.planb_backend.domain.trip.entity.Space;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
